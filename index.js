57d38f0a8b5190aa8355079d6a481a5acd2cb50e
Franky Liu <cuifranky95@gmail.com>
2019-07-27 05:15:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing (katzDeliLine) {
   }
 }
 function currentLine (katzDeliLine) {
-    if(katzDeliLine.length < 1) {
+    if(katzDeliLine.length === 0) {
       return "The line is currently empty."
     } else {
       var line = "The line is currently: "
